476515fd94fef9916ba46827d76f5a9e1bb7e356
Taeyeong Kim <xodud7737@gmail.com>
2025-02-04 19:09:11 +0900

    _

---
+++ 0. Basic/index.js
@@ -0,0 +1,14 @@
+// ``(back-ticks) also available!
+
+
+//console.log(`Hello`);
+//console.log(`I like pizza!`);
+
+// pop-up
+//window.alert(`This is an alert!`);
+//window.alert(`I like pizza!`);
+
+// document means 'our webpage'
+// below code change content which Id's "myH1"
+document.getElementById("myH1").textContent = 'Hello';
+document.getElementById("myP").textContent = "I like pizza!"
