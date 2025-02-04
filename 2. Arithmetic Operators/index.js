0a2da6e5770a9c60814a6654ecf490dbde0490a0
Taeyeong Kim <xodud7737@gmail.com>
2025-02-04 20:05:08 +0900

    _

---
+++ 2. Arithmetic Operators/index.js
@@ -0,0 +1,27 @@
+// arithmetic operators = operands (values, variables, etc.)
+//                        operator (+ - * /)
+//                        ex. 11 = x + 5;
+
+let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+
+// power of n
+students = students ** 2;
+
+// modular
+students = students %2;
+
+// shorcut(all possible)
+students += 2;
+students -= 2;
+students *= 2;
+students /= 2;
+students **= 2;
+students %= 2;
+
+// 30:22
+console.log(students);
