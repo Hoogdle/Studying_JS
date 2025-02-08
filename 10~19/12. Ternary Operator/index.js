ca5bde56ccfd83a4c676aa62c0b11c908a14abc9
Taeyeong Kim <xodud7737@gmail.com>
2025-02-08 17:37:59 +0900

    _

--- 10~19/12. Ternary Operator/index.js
+++ 10~19/12. Ternary Operator/index.js
@@ -3,5 +3,18 @@
 //                    condition ? codeIfTrue : codeIfFalse
 
 let age = 21;
-age >= 18 ? 
+let message = age >= 18 ? "You're an adult" : "You're a minor"
 
+console.log(message)
+
+let time = 16;
+let greeting = time < 12 ? "Good morning!" : "Good afternoon!"
+console.log(greeting)
+
+let isStudent = true;
+let message2 = isStudent ? "You are a student" : "You are NOT a student"
+console.log(message2)
+
+let purchaseAmount = 125;
+let discount = purchaseAmount >= 100 ? 10 : 0
+console.log(`Your total is ${purchaseAmount - purchaseAmount * (discount/100)} `)
