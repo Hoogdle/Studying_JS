df8958739717b15ecf5e6114641578ab0918ed7e
Taeyeong Kim <xodud7737@gmail.com>
2025-02-10 15:57:04 +0900

    _

---
+++ 10~19/16. Method Chaining/index.js
@@ -0,0 +1,22 @@
+// Method Chaining = Calling one method after another
+//                   in one continuous line of code.
+
+// ====== NO METHOD CHAINING ======
+
+let username = window.prompt("Enther your username : ")
+
+username = username.trim()
+
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
+
+let extraChar = username.slice(1);
+extraChar = extraChar.toLowerCase();
+username = letter + extraChar;
+
+console.log(username)
+
+// ====== METHOD CHAINING ======
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLocaleLowerCase()
+console.log(username)
