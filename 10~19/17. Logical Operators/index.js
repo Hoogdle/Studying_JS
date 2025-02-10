1559028937e48cc1aea32c5f15bb39b123afe9f1
Taeyeong Kim <xodud7737@gmail.com>
2025-02-10 16:05:23 +0900

    _

---
+++ 10~19/17. Logical Operators/index.js
@@ -0,0 +1,15 @@
+// Logical Operators = used to combine or manipulate boolean values
+//                      (true or false)
+
+//                      And = &&
+//                      Or = ||
+//                      Not = !
+
+const temp = 25;
+
+if(temp > 0 && temp <= 30){
+    console.log(`The weather is Good!`)
+}
+else{
+    console.log(`The weather is Bad!`)
+}
